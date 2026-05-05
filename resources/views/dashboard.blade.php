be84b87173ed7040c0ba4924e7d88a5b4a4e4597
nurulftmh <77710297+nurulftmh@users.noreply.github.com>
2026-05-05 14:15:33 +0800

    update fitur

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
                 <form action="{{ route('attendance.checkin') }}" method="POST">
                     @csrf
                     <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">
-                        Absen Masuk
+                        Masuk
                     </button>
                 </form>
             @else
